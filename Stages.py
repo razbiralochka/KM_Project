58318443d6520b16e67bd7f27de3f17d975e8bda
razbiralochka <razbiralochka@yandex.ru>
2023-04-04 20:01:26 +0400

    added parts to stage

--- Stages.py
+++ Stages.py
@@ -19,7 +19,7 @@ class Stage:
         return res
     def stage_inertia(self):
         for i, part in enumerate(self._stage_parts):
-            self._stage_inertia+=part.part_inertia()+part.part_mass()*self._lenght_of_parts[i]**2
+            self._stage_inertia+=part.part_inertia()+part.part_mass()*(self._lenght_of_parts[i]+part.CoM())**2
         return self._stage_inertia
 
 
@@ -28,22 +28,49 @@ class Stage:
 
 upper_stage = Stage()
 
-upper_stage.add_part_to_stage(Fairing(2.9, 1.7, 200))
-upper_stage.add_part_to_stage(Payload(1000))
-upper_stage.add_part_to_stage(Adapter(1.7, 1.7, 0.6, 200))
-upper_stage.add_part_to_stage(Fuel_tank(1.7, 1.48, 105.031, liquid_type="LOX"))
-upper_stage.add_part_to_stage(Fuel_tank(1.7, 1.1, 367.607,  liquid_type="LNG"))
-upper_stage.add_part_to_stage(Engine(3700, 15.9))
-upper_stage.add_part_to_stage(Adapter(1.7,1.7,2.1,200))
+upper_stage.add_part_to_stage(Fairing(lenght=2.9,
+                                      dia=1.7,
+                                      mass=200))
+upper_stage.add_part_to_stage(Payload(mass = 1000))
+upper_stage.add_part_to_stage(Adapter(upper_dia = 1.7,
+                                      lower_dia = 1.7,
+                                      lenght = 0.6,
+                                      mass = 200))
+upper_stage.add_part_to_stage(Fuel_tank(dia=1.7,
+                                        lenght=1.48,
+                                        constr_mass=105.031,
+                                        liquid_type="LOX"))
+upper_stage.add_part_to_stage(Fuel_tank(dia=1.7,
+                                        lenght=1.1,
+                                        constr_mass=367.607,
+                                        liquid_type="LNG"))
+upper_stage.add_part_to_stage(Engine(imp=3700,
+                                     cunsuption=15.9))
+upper_stage.add_part_to_stage(Adapter(upper_dia=1.7,
+                                      lower_dia=1.7,
+                                      lenght=2.1,
+                                      mass=200))
 
 lower_stage = Stage()
-lower_stage.add_part_to_stage(Fuel_tank(1.7, 8.39, 435.02, liquid_type="LOX"))
-lower_stage.add_part_to_stage(Fuel_tank(1.7, 6.21, 1522.58,  liquid_type="LNG"))
-lower_stage.add_part_to_stage(Engine(3300, 120))
-lower_stage.add_part_to_stage(Adapter(1.7, 1.7, 2.8, 300))
+lower_stage.add_part_to_stage(Fuel_tank(dia=1.7,
+                                        lenght=8.39,
+                                        constr_mass=435.02,
+                                        liquid_type="LOX"))
+lower_stage.add_part_to_stage(Fuel_tank(dia=1.7,
+                                        lenght=6.21,
+                                        constr_mass=1522.58,
+                                        liquid_type="LNG"))
+lower_stage.add_part_to_stage(Engine(imp=3300,
+                                     cunsuption=120))
+lower_stage.add_part_to_stage(Adapter(upper_dia=1.7,
+                                      lower_dia=1.7,
+                                      lenght=2.8,
+                                      mass=300))
 
 
+print("Масса 2-го РБ ", upper_stage.stage_mass())
+print("Масса 1-го РБ ", lower_stage.stage_mass())
+print("Длина ракеты: ",upper_stage.stage_lenght()+lower_stage.stage_lenght())
 
-print(lower_stage.stage_mass())
-print(lower_stage.stage_lenght())
-print(lower_stage.stage_inertia())
+print("Инерция 2-го РБ: ",upper_stage.stage_inertia())
+print("Инерция 1-го РБ: ",lower_stage.stage_inertia())
--- parts.py
+++ parts.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 class Fuel_tank:
-    def __init__(self, dia, lenght, constr_mass , liquid_type = "LNG"):
+    def __init__(self, dia = 0, lenght = 0, constr_mass  = 0, liquid_type = "LNG"):
         self._diameter  = dia
         self._radius = dia / 2
         self._lenght = lenght
@@ -19,6 +19,7 @@ class Fuel_tank:
         self._liquid_lenght = 0.9*lenght
         self._liquid_volume = self._liquid_lenght*np.pi*(dia**2)/4
         self._liquid_mass = self._liquid_density*self._liquid_volume
+        print(liquid_type," ",self._liquid_mass)
         self._total_mass = self._constr_mass + self._liquid_mass
         self._constr_inertia = self._constr_mass * ((self._radius ** 2) / 2 + (self._lenght ** 2) / 3)
     def burnout(self, consumption, time):
@@ -47,7 +48,7 @@ class Fuel_tank:
 
 
 class Engine:
-    def __init__(self, imp, cunsuption):
+    def __init__(self, imp = 0, cunsuption = 0):
         self._imp = imp
         self._cunsuption = cunsuption
         self._thrust = imp*cunsuption
@@ -65,12 +66,12 @@ class Engine:
     def CoM(self):
         return 0
 class Adapter:
-    def __init__(self, upper_dim, lower_dim, lenght, mass):
-        self._upper_dim = upper_dim
-        self._lower_dim = lower_dim
+    def __init__(self, upper_dia =0 , lower_dia = 0, lenght = 0, mass = 0):
+        self._upper_dim = upper_dia
+        self._lower_dim = lower_dia
         self._lenght = lenght
-        r1 = upper_dim/2
-        r2 = lower_dim/2
+        r1 = upper_dia/2
+        r2 = lower_dia/2
         self._inertia = (lenght**2)*mass/(r1+3*r2)/(6*(r1+r2))+0.25*mass*(pow(r2, 2)+pow(r1, 2))
         self._CoM = self._lenght/3+(2*self._lower_dim+self._upper_dim)/(self._lower_dim+self._upper_dim)
         self._mass = mass
@@ -85,31 +86,30 @@ class Adapter:
     def CoM(self):
         return self._CoM
 class Fairing:
-    def __init__(self, lenght, dim, mass):
+    def __init__(self, lenght=0, dia=0, mass=0):
         self._lenght = lenght
-        self._dim = dim
-        self._radius = dim/2
+        self._dim = dia
+        self._radius = dia/2
         self._mass = mass
         self._static = 0
         self._density = 0
         self.upd_masses()
-
     def radius(self,arg):
         res =0.5*((self._radius**2-self._lenght**2)/self._radius+np.sqrt(pow(self._lenght,4)/pow(self._radius,2)
                                                                     +pow(self._radius,2)-2*pow(self._lenght,2)+
                                                                     8*self._lenght*arg-4*pow(arg,2)))
         return res
-
     def inert(self,arg):
         res = (pow(self.radius(arg), 2) / 2 + pow(arg, 2)) * self._density * self.radius(arg)
-
         return res
     def upd_masses(self):
         l = np.linspace(0, self._lenght, 10000)
         surface = 2 * np.pi * np.trapz(self.radius(l), l)
         self._density = self._mass / surface
         self._inertia = np.trapz(self.inert(l), l)
-        self._static = self._density * 2 * np.pi * np.trapz(self.radius(l) * l, l) / self._mass
+        self._static  = self._density * 2 * np.pi * np.trapz(self.radius(l) * l, l) / self._mass
+        print('Инерция ГО: ', self._inertia)
+        print('Центр тяжести ГО: ',self._static)
 
     def part_inertia(self):
         return self._inertia
@@ -123,7 +123,7 @@ class Fairing:
         return  self._static
 
 class Payload:
-    def __init__(self, mass):
+    def __init__(self, mass =0):
         self._mass = mass
 
     def part_mass(self):
